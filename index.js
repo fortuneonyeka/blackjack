860f965df17afd81cf5e5d8b711bf6912235d7d5
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-05-20 12:46:09 +0100

    added the condition to check the ACE,KING AND QUEEN

--- index.js
+++ index.js
@@ -1,52 +1,53 @@
-let firstCard = generateRandomInteger()
-let secondCard = generateRandomInteger()
-const cards = [firstCard,secondCard]
+let firstCard = generateRandomInteger();
+let secondCard = generateRandomInteger();
+const cards = [firstCard, secondCard];
 
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
-let message = ""
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
-
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
 function generateRandomInteger() {
-  let randonNumber = Math.floor(Math.random() * 11) + 1;
-  return randonNumber
+  let randomNumber = Math.floor(Math.random() * 11) + 1;
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  }
+  return randomNumber;
 }
 // Create a new function called startGame() that calls renderGame()
 const startGame = () => {
-  renderGame()
-}
-
+  renderGame();
+};
 
 function renderGame() {
-  cardsEl.textContent = "Cards: "
-    
-    for (let i = 0; i < cards.length; i++) {
-      cardsEl.textContent += cards[i] + " "
-      
-    }
+  cardsEl.textContent = "Cards: ";
 
-    sumEl.textContent = "Sum: " + sum
-    if (sum <= 20) {
-        message = "Do you want to draw a new card?"
-    } else if (sum === 21) {
-        message = "You've got Blackjack!"
-        hasBlackJack = true
-    } else {
-        message = "You're out of the game!"
-        isAlive = false
-    }
-    messageEl.textContent = message
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+
+  sumEl.textContent = "Sum: " + sum;
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
 }
 
-
 function newCard() {
-    let card = generateRandomInteger()
-    sum += card
+  let card = generateRandomInteger();
+  sum += card;
 
-    cards.push(card)
-    renderGame()
+  cards.push(card);
+  renderGame();
 }
